f86d842b6b94db92db9e6173fd7eea0e2308fbf9
TripleHeaven <heaven30.buroff@gmail.com>
2023-01-17 21:54:32 +0300

    first task

---
+++ 1/1_2.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char * argv[]) {
+
+    cout << "main started " << getpid() << " " << getppid()<< endl;
+
+    for (int i = 0 ; i <= atoi(argv[1]); i++) {
+        cout << i << endl;
+        if (!fork()) {
+            cout << "child started pipID = "<< getpid() << " PPID =" << getppid() << " i=" << i <<  endl;
+            sleep(5);
+            cout << "child finished pipID =" << getpid() << " PPID=" << getppid() <<  endl;
+            exit(1);
+        }
+    }
+
+    for (int i = 0 ; i <= atoi(argv[1]); i++) {
+        wait(0);
+    }
+
+    cout << "main finished" << " PID=" <<getpid() << " PPID="<< getppid()<< endl;
+
+
+    return 0;
+}
